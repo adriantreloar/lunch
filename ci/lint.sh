179af6d92f758763b2903562e2a52c6a4e5d4405
adriantreloar <adrian.treloar@cubeshine.com>
2022-04-07 09:35:17 +0100

    Typed out lint.sh

---
+++ ci/lint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Run all code linters
+# Always exit 0, even in the case of error
+
+# Move to the project root directory
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+
+set -o xtrace
+
+# Sort imports
+isort .
+# Make the code look like a Nazi would find appealing
+black -t py38 .  # py39 not yet available
+flake8
+mypy lunch

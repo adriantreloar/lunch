0b5a9d6e60a7afa06e2ea35060b38e5f64d9ec06
adriantreloar <adrian.treloar@cubeshine.com>
2022-04-13 19:05:33 +0100

    corrected yaml_model_serializer.py

--- lunch/storage/serialization/yaml_model_serializer.py
+++ lunch/storage/serialization/yaml_model_serializer.py
@@ -11,7 +11,7 @@ class YamlModelSerializer(ModelSerializer):
     def __init__(self, persistor: LocalFileModelPersistor):
         super.__init__(persistor)
 
-    async def get_dimension(self, name:str, version: Version) -> dict:
+    async def get_dimension(self, name: str, version: Version) -> dict:
         """
 
         :param name: Name of the dimension
@@ -30,16 +30,15 @@ class YamlModelSerializer(ModelSerializer):
         _put_dimension(dimension, version, self._persistor)
 
 
-async def _get_dimension(dimension: dict, version: Version, persistor: LocalFileModelPersistor):
-    with await persistor.open_dimension_file_read(dimension["name"], version.model_version) as stream:
-        dict = await yaml.load(stream)
-    return dict
+async def _get_dimension(name: str, version: Version, persistor: LocalFileModelPersistor):
+    with await persistor.open_dimension_file_read(name=name, version=version.model_version) as stream:
+        d = await yaml.load(stream)
+    return d
 
 
 async def _put_dimension(dimension: dict, version: Version, persistor: LocalFileModelPersistor):
-
-    with await persistor.open_dimension_file_write(dimension["name"], version.model_version) as stream:
-        await yaml.dump(dict, stream)
+    with await persistor.open_dimension_file_write(name=dimension["name"], version=version.model_version) as stream:
+        await yaml.dump(dimension, stream)
 
 
 

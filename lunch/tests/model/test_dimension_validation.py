48194aa84c3127144af8a9da0bbfd8a048b0523a
adriantreloar <adrian.treloar@cubeshine.com>
2022-04-28 08:47:50 +0100

    Added failing tests to test_dimension_validation.py attributes and id_ checks

--- lunch/tests/model/test_dimension_validation.py
+++ lunch/tests/model/test_dimension_validation.py
@@ -13,6 +13,63 @@ def test_can_contain_name_key():
     dimension = {"name": "Department"}
     assert dim_valid.validate(dimension)
 
+@pytest.mark.parametrize(
+    "comment, id_",
+    [
+        ("int", 1),
+        ("None", None),
+    ]
+)
+def test_can_contain_id(comment, id_):
+    dimension = {"name": "Department", "id_": id_}
+    assert dim_valid.validate(dimension)
+
+@pytest.mark.parametrize(
+    "comment, id_",
+    [
+        ("string", "foo"),
+        ("dict", {1: 2}),
+        ("empty_set", set()),
+        ("empty_dict", {}),
+        ("empty_string", ""),
+    ]
+)
+def test_id_must_be_int(comment, id_):
+    dimension = {"name": "Department", "id_": id_}
+    with pytest.raises(DimensionValidationError):
+        dim_valid.validate(dimension)
+
+@pytest.mark.parametrize(
+    "comment, attributes",
+    [
+        ("empty_list", []),
+        ("list", [{"name":"foo"},{"id_":1}]),
+        ("None", None),
+    ]
+)
+def test_can_contain_attributes_key(comment, attributes):
+    assert comment
+    dimension = {"name": "Department", "attributes": attributes}
+    assert dim_valid.validate(dimension)
+
+
+@pytest.mark.parametrize(
+    "comment, attributes",
+    [
+        ("string", "foo"),
+        ("int", 1),
+        ("dict", {1: 2}),
+        ("empty_set", set()),
+        ("empty_dict", {}),
+        ("empty_string", ""),
+    ]
+)
+def test_attributes_key_must_be_allowed_type(comment, attributes):
+    assert comment
+    dimension = {"name": "Department", "attributes": attributes}
+    with pytest.raises(DimensionValidationError):
+        dim_valid.validate(dimension)
+
 
 def test_must_contain_name_key():
     dimension = {"foo": "Department"}

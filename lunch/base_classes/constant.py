db5738cc495244971d4718b7316840dcf7e35267
adriantreloar <adrian.treloar@cubeshine.com>
2022-05-20 09:00:33 +0100

    Added global (constant) state

---
+++ lunch/base_classes/constant.py
@@ -0,0 +1,12 @@
+class Constant:
+    """A base class, for classes which hold constant data.
+
+    Constants hold unchanging state
+
+    Tests for constants should only need to test that any functions that pull out part of a globals
+    return the correct part.
+    e.g.
+    >>> assert my_constant.constant_tree.leaves == ['leaf1', 'leaf2']
+    """
+
+    pass
---
+++ lunch/globals/global_state.py
@@ -0,0 +1,9 @@
+from lunch.base_classes.constant import Constant
+
+class GlobalState(Constant):
+
+    @staticmethod
+    @property
+    def default_dimension_storage(self):
+        return {"storage": "columnar"}
+    

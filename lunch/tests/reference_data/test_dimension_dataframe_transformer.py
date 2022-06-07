c2fe261754109a19987141b02c6310c886b7cd64
adriantreloar <adrian.treloar@cubeshine.com>
2022-06-07 09:05:01 +0100

    test_columnize written and failing

--- lunch/tests/reference_data/test_dimension_dataframe_transformer.py
+++ lunch/tests/reference_data/test_dimension_dataframe_transformer.py
@@ -26,8 +26,7 @@ async def test_make_dataframe():
     assert result[2].tolist() == [5,4,3,2,1]
 
 
-@pytest.mark.asyncio
-async def test_basic_merge():
+def test_basic_merge():
 
     compare_df = DataFrame([{"a": "foo", "b": 10}, {"a": "bar", "b": 20}])
 
@@ -48,7 +47,65 @@ async def test_basic_merge():
     assert (expected["a"] == result["a"]).all(), (expected, result)
     assert (expected["b"] == result["b"]).all(), (expected, result)
 
+def test_merge_with_int_columns():
+
+    compare_df = DataFrame([{1: "foo", 2: 10}, {1: "bar", 2: 20}])
+
+    source_df = DataFrame([{1: "bar", 2: 21}, {1: "baz", 2: 30}])
+
+    key = [1]
+
+    result = DimensionDataFrameTransformer.merge(source_df=source_df,
+                                                compare_df=compare_df,
+                                                key=key)
+
+    # NOTE - the result has been sorted by key
+    expected = DataFrame([{1: "bar", 2: 21},
+                          {1: "baz", 2: 30},
+                          {1: "foo", 2: 10},
+                          ])
+
+    assert (expected[1] == result[1]).all(), (expected, result)
+    assert (expected[2] == result[2]).all(), (expected, result)
+
+def test_merge_with_id_column():
+
+    compare_df = DataFrame([{-1: 0, 1: "foo", 2: 10}, {-1: 1, 1: "bar", 2: 20}])
+
+    source_df = DataFrame([{1: "bar", 2: 21}, {1: "baz", 2: 30}])
+
+    key = [1]
+
+    result = DimensionDataFrameTransformer.merge(source_df=source_df,
+                                                compare_df=compare_df,
+                                                key=key)
+
+    # NOTE - the result has been sorted by key
+    expected = DataFrame([{-1: 1, 1: "bar", 2: 21},
+                          {-1: np.NaN, 1: "baz", 2: 30},
+                          {-1: 0, 1: "foo", 2: 10},
+                          ])
+
+    assert (expected[-1].fillna(-1) == result[-1].fillna(-1)).all(), (expected, result)
+    assert (expected[1] == result[1]).all(), (expected, result)
+    assert (expected[2] == result[2]).all(), (expected, result)
+
 @pytest.mark.asyncio
 async def test_columnize():
+    # dictionary of columns? attribute_id : column/iterator
+    # index is -1?
+    # columnize(data: pd.DataFrame) -> dict[int: Iterable]
 
-    assert False
+    source_df = DataFrame([{-1: 1, 1: "bar", 2: 21},
+                          {-1: np.NaN, 1: "baz", 2: 30},
+                          {-1: 0, 1: "foo", 2: 10},
+                          ])
+
+    result = DimensionDataFrameTransformer.columnize(data=source_df)
+
+    assert list(result[-1][0] == 1)
+    assert list(np.isnan(result[-1][1]))
+    assert list(result[-1][2] == 0)
+
+    assert list(result[1]) == ["bar", "baz", "foo"]
+    assert list(result[2]) == [21, 30, 10]

4265b2e181072ed3164f0213b9c17d6d5505e45d
adriantreloar <adrian.treloar@cubeshine.com>
2022-11-12 18:31:54 +0000

    refactored test_put_dimensions_first_insert ready for parametrization

--- lunch/tests/storage/test_model_store.py
+++ lunch/tests/storage/test_model_store.py
@@ -16,8 +16,8 @@ from lunch.storage.transformers.dimension_index_transformer import (
 from lunch.storage.transformers.fact_index_transformer import FactIndexTransformer
 
 
-@pytest.mark.asyncio
-async def test_put_indexes_when_putting_dimensions():
+@pytest.fixture()
+def null_cache_model_store():
 
     # Don't mock these transformers
     dimension_transformer = DimensionTransformer()
@@ -40,27 +40,11 @@ async def test_put_indexes_when_putting_dimensions():
         serializer=serializer,
         cache=cache,
     )
+    yield testee_model_store, serializer, cache
 
-    read_version = Version(
-        version=1,
-        model_version=1,
-        reference_data_version=0,
-        cube_data_version=0,
-        operations_version=0,
-        website_version=0,
-    )
-    write_version = Version(
-        version=2,
-        model_version=2,
-        reference_data_version=0,
-        cube_data_version=0,
-        operations_version=0,
-        website_version=0,
-    )
 
-    # TODO test dimensions with and without ids
-    # TODO test updates to dimension
-
+@pytest.fixture()
+def d_test():
     d_test = {
         "name": "Test",
         "attributes": [
@@ -72,13 +56,68 @@ async def test_put_indexes_when_putting_dimensions():
             "foo",
         ],
     }
+    yield d_test
 
-    serializer.get_dimension_version_index.return_value = {1: 1}
-    serializer.get_dimension_name_index.return_value = {}
-    serializer.get_max_dimension_id.return_value = 0
+
+@pytest.fixture()
+def v_0_1_2():
+    v0 = Version(
+        version=0,
+        model_version=0,
+        reference_data_version=0,
+        cube_data_version=0,
+        operations_version=0,
+        website_version=0,
+    )
+    v1 = Version(
+        version=1,
+        model_version=1,
+        reference_data_version=0,
+        cube_data_version=0,
+        operations_version=0,
+        website_version=0,
+    )
+    v2 = Version(
+        version=2,
+        model_version=2,
+        reference_data_version=0,
+        cube_data_version=0,
+        operations_version=0,
+        website_version=0,
+    )
+    yield v0, v1, v2
+
+
+@pytest.mark.asyncio
+async def test_put_dimensions_first_insert(null_cache_model_store, v_0_1_2, d_test):
+
+    testee_model_store, serializer, _ = null_cache_model_store
+    _, v1, v2 = v_0_1_2
+
+    put_dimensions = [d_test]
+    read_version = v1
+    write_version = v2
+
+    read_version_index = {}
+    read_name_index = {}
+    read_max_dimension_id = 0
+
+    # Model version and id have been added to dimension
+    written_dimensions = [{**d_test, **{"id_": 1, "model_version": 2}}]
+    written_name_index = {"Test": 1}
+    written_version_index = {1: 2}  # dimension 1 is at version 2
+
+    # TODO test dimensions with and without ids
+    # TODO test updates to dimension
+
+    serializer.get_dimension_version_index.return_value = read_version_index
+    serializer.get_dimension_name_index.return_value = read_name_index
+    serializer.get_max_dimension_id.return_value = read_max_dimension_id
 
     await testee_model_store.put_dimensions(
-        dimensions=[d_test], read_version=read_version, write_version=write_version
+        dimensions=put_dimensions,
+        read_version=read_version,
+        write_version=write_version,
     )
 
     # Check that all of the gets have been called with the read version
@@ -86,27 +125,13 @@ async def test_put_indexes_when_putting_dimensions():
     serializer.get_dimension_version_index.assert_called_with(version=read_version)
     serializer.get_dimension_name_index.assert_called_with(version=read_version)
 
+    # Check that all of the puts have been put with the write version
     serializer.put_dimension_version_index.assert_called_with(
-        index_={1: 2}, version=write_version
+        index_=written_version_index, version=write_version
     )
     serializer.put_dimension_name_index.assert_called_with(
-        index_={"Test": 1}, version=write_version
+        index_=written_name_index, version=write_version
     )
-
-    # NOTE: model version and id have been added to dimension
-    written_dimension = {
-        "id_": 1,
-        "model_version": 2,
-        "name": "Test",
-        "attributes": [
-            {"name": "foo"},
-            {"name": "bar"},
-            {"name": "baz"},
-        ],
-        "key": [
-            "foo",
-        ],
-    }
     serializer.put_dimensions.assert_called_with(
-        dimensions=[written_dimension], version=write_version
+        dimensions=written_dimensions, version=write_version
     )

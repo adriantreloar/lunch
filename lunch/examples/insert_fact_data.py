250c2c810e6e054c43940db49b0092714df88c21
adriantreloar <adrian.treloar@cubeshine.com>
2022-11-25 12:53:59 +0000

    notes in save_fact_data

--- lunch/examples/insert_fact_data.py
+++ lunch/examples/insert_fact_data.py
@@ -1,8 +1,17 @@
-from lunch.examples.insert_dimension_data import save_dimension
+from lunch.examples.insert_dimension_data import insert_dimension_data
 from lunch.examples.save_fact import save_fact
 
 async def save_fact_data():
 
-    await save_dimension()
+    await insert_dimension_data()
     await save_fact()
+
+    # TODO save_fact not actually creating the fact table...
+
+    # TODO, change insert_dimension_data to insert some of the other dimensions
+
+    # Create some fact data
+
+    # append it to f_sales
+
 
